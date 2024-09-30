3d5b8716955f07a51155e64982c0d96691fb1b28
Jelp200 <jorge.pena0516@gmail.com>
2024-09-29 21:54:07 -0600

    :croissant: [MOD] TESTLED.c

--- PRUEBAS/Attiny84/testLEDuChipStdio/ledBlinkC/ledBlinkC/main.c
+++ PRUEBAS/Attiny84/testLEDuChipStdio/ledBlinkC/ledBlinkC/main.c
@@ -14,14 +14,17 @@ int main(void)
 {
 	//* Pin PA0 como salida
 	DDRA |= (1 << DDA0);
+	DDRA |= (1 << DDA1);
+	
 	
     /* Replace with your application code */
     while (1) 
     {
+		PORTA |= (1 << PORTA1);
 		PORTA |= (1 << PORTA0);				// Pin alto (LED encendido)
-		_delay_ms(1000);					// 1s delay
+		_delay_ms(3000);					// 1s delay
 		PORTA &= ~(1 << PORTA0);			// Pin bajo (LED apagado)
-		_delay_ms(1000);					// 1s delay
+		_delay_ms(3000);					// 1s delay
     }
 }
 
