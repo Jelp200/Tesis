5c8bc48a1c63b78cc282a4ef474496fd87e31bac
Jelp200 <jorge.pena0516@gmail.com>
2024-09-29 20:59:59 -0600

    :croissant: [ADD] TESTLED.c

---
+++ PRUEBAS/Attiny84/testLEDuChipStdio/ledBlinkC/ledBlinkC/main.c
@@ -0,0 +1,27 @@
+/*
+ * ledBlinkC.c
+ *
+ * Created: 29/09/2024 07:18:53 p. m.
+ * Author : nikob
+ */ 
+
+#define F_CPU	1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	//* Pin PA0 como salida
+	DDRA |= (1 << DDA0);
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTA |= (1 << PORTA0);				// Pin alto (LED encendido)
+		_delay_ms(1000);					// 1s delay
+		PORTA &= ~(1 << PORTA0);			// Pin bajo (LED apagado)
+		_delay_ms(1000);					// 1s delay
+    }
+}
+
