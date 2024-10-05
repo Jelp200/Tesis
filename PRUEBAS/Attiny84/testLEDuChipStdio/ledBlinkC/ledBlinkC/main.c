cc8fa1b3a517ea9e59b8617443cae6766a00cfbd
Jelp200 <jorge.pena0516@gmail.com>
2024-10-05 15:32:01 -0600

    :croissant: [MOD] TESTLED.c

--- PRUEBAS/Attiny84/testLEDuChipStdio/ledBlinkC/ledBlinkC/main.c
+++ PRUEBAS/Attiny84/testLEDuChipStdio/ledBlinkC/ledBlinkC/main.c
@@ -1,29 +1,38 @@
 /*
- * ledBlinkC.c
- *
- * Created: 29/09/2024 07:18:53 p. m.
- * Author : nikob
- */ 
+** #########################################################################################
+**  Archivo         :   main.c
+**  Proyecto        :   Tesis
+**  Herramienta(s)  :   Microchip Studio
+**  Compilador      :   C/C++ Compiler
+**  Autores         :
+**                      Jorge E. Peña P.
+**  Descripcion     :
+**                      Control de LED's (Fotorreceptor, fototransmisor, LED verde).
+** #########################################################################################
+*/
 
+//* DEFINICIÓN DE VELOCIDAD DEL RELOJ (1MHz)
 #define F_CPU	1000000UL
+
+//* DIRECTIVAS DE PREPROCESADOR
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-int main(void)
-{
-	//* Pin PA0 como salida
-	DDRA |= (1 << DDA0);
-	DDRA |= (1 << DDA1);
-	
+//* FUNCIÓN PRINCIPAL
+int main(void) {
+	// Configuración de pines de salida
+	DDRA |= (1 << DDA0);					// PA0 como salida
+	DDRA |= (1 << DDA1);					// PA1 como salida
+	DDRA |= (1 << DDA2);					// PA2 como salida
 	
     /* Replace with your application code */
-    while (1) 
-    {
-		PORTA |= (1 << PORTA1);
-		PORTA |= (1 << PORTA0);				// Pin alto (LED encendido)
+    while(1) {
+		PORTA |= (1 << PORTA1);				// Pin 1 alto (LED siempre encendido)
+		PORTA &= ~(1 << PORTA2);			// Pin 2 bajo (LED apagado)
+		PORTA |= (1 << PORTA0);				// Pin 0 alto (LED encendido)
 		_delay_ms(3000);					// 1s delay
-		PORTA &= ~(1 << PORTA0);			// Pin bajo (LED apagado)
+		PORTA |= (1 << PORTA2);				// Pin 2 alto (LED encendido)
+		PORTA &= ~(1 << PORTA0);			// Pin 0 bajo (LED apagado)
 		_delay_ms(3000);					// 1s delay
     }
 }
